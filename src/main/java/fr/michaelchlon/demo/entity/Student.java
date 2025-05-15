0a520e30029b3b0f860bf65e1a142f312db0cfc4
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-05-15 14:50:33 +0200

    feat(REST CRUD API): Spring Boot REST POJO - PART 1 -
    feat: add Student entity and REST test file
    
    - Add Student entity with fields and methods
    - Introduce rest.http for API testing
    - Fix indentation in DemoApplication main method

---
+++ src/main/java/fr/michaelchlon/demo/entity/Student.java
@@ -0,0 +1,32 @@
+package fr.michaelchlon.demo.entity;
+
+public class Student {
+  private String firstName;
+  private String lastName;
+
+  // * INFO: ╾╼ CONSTRUCTOR ╾───────────────────────────────────────────────────╼
+  public Student() {
+  }
+
+  public Student(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  // * INFO: GETTERS AND SETTERS ______________________________________________
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+}
