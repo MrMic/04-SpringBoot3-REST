f790f29fc0748420dd8a10aae45ea9792ff3a870
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-05-17 15:05:38 +0200

    feat(REST CRUD API): Spring Boot REST Path variables - PART 1 -
    feat: initialize student data with @PostConstruct
    
    - Add @PostConstruct method to preload student data
    - Refactor student list initialization to loadData method

--- src/main/java/fr/michaelchlon/demo/rest/StudentRestController.java
+++ src/main/java/fr/michaelchlon/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package fr.michaelchlon.demo.rest;
 
 import fr.michaelchlon.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+  private List<Student> theStudents;
+
+  // define @PostConstruct to load student data ... only once!
+  @PostConstruct
+  public void loadData() {
+    theStudents = new ArrayList<>();
+    theStudents.add(new Student("John", "Doe"));
+    theStudents.add(new Student("Mary", "Smith"));
+    theStudents.add(new Student("Peter", "Jackson"));
+  }
 
   // define endpoint for "/students" - return a list of students
   @GetMapping("/students")
   public List<Student> getStudents() {
-    List<Student> theStudents = new ArrayList<>();
-    theStudents.add(new Student("John", "Doe"));
-    theStudents.add(new Student("Mary", "Smith"));
-    theStudents.add(new Student("Peter", "Jackson"));
 
     return theStudents;
   }
